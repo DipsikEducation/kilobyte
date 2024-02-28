53c4c5367fc90351a4a62d65ac86d57a88f47424
DipsikKk <141740406+DearDany@users.noreply.github.com>
2024-02-29 00:12:34 +0200

    start project

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/*LIBRARIES
+1. Axios
+(docs) - https://www.npmjs.com/package/axios?activeTab=readme
+2.izitoast
+(docs) - https://www.npmjs.com/package/izitoast
+3.tui-pagination
+(docs) - https://www.npmjs.com/package/tui-pagination
+4.Swiper 
+(docs) - https://www.npmjs.com/package/swiper
+*/ 
