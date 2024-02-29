92a2c6165df6c07f30fe5f042d6b2c5ff165fc1b
MaxBeliienko <mbelienko@gmail.com>
2024-02-29 10:37:24 +0200

    fetchApi
    Трішки скоротив код попереднього коміту

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://books-backend.p.goit.global/books/';
+
 export async function fetchTopBooks() {
-  return await axios.get('https://books-backend.p.goit.global/books/top-books');
+  return await axios.get(`${BASE_URL}top-books`);
 }
 
 export async function fetchCategoriesList() {
-  return await axios.get(
-    'https://books-backend.p.goit.global/books/category-list'
-  );
+  return await axios.get(`${BASE_URL}category-list`);
 }
 
 export async function fetchSelectedCategory(category) {
-  return await axios.get(
-    `https://books-backend.p.goit.global/books/category?category=${category}`
-  );
+  return await axios.get(`${BASE_URL}category?category=${category}`);
 }
 
 export async function fetchBookDetails(bookId) {
-  return await axios.get(`https://books-backend.p.goit.global/books/${bookId}`);
+  return await axios.get(`${BASE_URL}${bookId}`);
 }
