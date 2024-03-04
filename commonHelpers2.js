50ffcc68f0132bcce0070e5ea33fe9c897a0e0a3
DipsikEducation <144449101+DipsikEducation@users.noreply.github.com>
2024-03-04 21:00:15 +0000

    Deploying to gh-pages from @ DipsikEducation/kilobyte@2f8037bb35a760a6667f06fa9fe32dfaa54c8dbb 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{g as i,d as p,e as n,h as l}from"./assets/localStorage-0834c673.js";import"./assets/vendor-b7a1aa6d.js";const r=document.querySelector("#shopping-list"),o=document.querySelector(".box-bookslist");s();function s(){i().length?o.classList.add("emptyShoppingList"):o.classList.remove("emptyShoppingList")}window.addEventListener("load",e);function e(){const t=i().map(a=>g(a)).join("");r.innerHTML=t,document.querySelector("#shopping-list").addEventListener("click",c)}function c(t){!t.target.hasAttribute("id")!=="icon-deleteBtn"&&(p(t),s(),e())}function g(t){return`<li class="shopping-list-item">
+import{g as i,d as l,e as p,h as r}from"./assets/localStorage-563e94f1.js";import"./assets/vendor-b7a1aa6d.js";const c=document.querySelector("#shopping-list"),e=document.querySelector(".box-bookslist");s();function s(){i().length?e.classList.add("emptyShoppingList"):e.classList.remove("emptyShoppingList")}window.addEventListener("load",n);function n(){const t=i().map(a=>g(a)).join("");c.innerHTML=t,document.querySelector("#shopping-list").addEventListener("click",d)}function d(t){!t.target.hasAttribute("id")!=="icon-deleteBtn"&&(l(t),s(),n())}function g(t){return`<li class="shopping-list-item">
     <button type="button" class="icon-deleteBtn" data-id="${t._id}">
       <svg class="icon-trash" id="icon-trash-03" width="28" height="28">
         <use href="/img/noptimizesprite.svg#icon-trash-03"></use>
@@ -15,13 +15,13 @@ import{g as i,d as p,e as n,h as l}from"./assets/localStorage-0834c673.js";impor
         <p class="book-author">${t.author}</p>
         <div class="img-wrap shopping-img-wrap">
           <a href="${t.amazon_product_url}" target="blank">
-            <img src="${n}" alt="amazon-logo" class="amazon-logo shopping-amazon-logo" width="62" height="19">
+            <img src="${p}" alt="amazon-logo" class="amazon-logo shopping-amazon-logo" width="62" height="19">
           </a>
           <a href="${t.buy_links[1].url}" target="_blank">
-            <img src="${l}" alt="apple-book" class="apple-logo shopping-apple-logo" width="33" height="32">
+            <img src="${r}" alt="apple-book" class="apple-logo shopping-apple-logo" width="33" height="32">
           </a>
         </div>
       </div>
     </div>
-  </li>`}
+  </li>`}window.addEventListener("resize",function(){h()});function h(){const t=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,o=document.querySelector(".support");t>1440&&o?o.style.display="block":o.style.display="none"}
 //# sourceMappingURL=commonHelpers2.js.map
