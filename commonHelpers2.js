f24c98099aefb5ba3ad7d878ad0b84bfd4fdf70d
DipsikEducation <144449101+DipsikEducation@users.noreply.github.com>
2024-03-04 12:21:56 +0000

    Deploying to gh-pages from @ DipsikEducation/kilobyte@791206bd416216b075d8229e363c0dcfdea05e42 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,27 @@
+import{g as i,r as p,a as n,b as l}from"./assets/main-009f9e52.js";import"./assets/vendor-b7a1aa6d.js";const r=document.querySelector("#shopping-list"),o=document.querySelector(".box-bookslist");s();function s(){i().length?o.classList.add("emptyShoppingList"):o.classList.remove("emptyShoppingList")}window.addEventListener("load",e);function e(){const t=i().map(a=>g(a)).join("");r.innerHTML=t,document.querySelector("#shopping-list").addEventListener("click",c)}function c(t){!t.target.hasAttribute("id")!=="icon-deleteBtn"&&(p(t),s(),e())}function g(t){return`<li class="shopping-list-item">
+    <button type="button" class="icon-deleteBtn" data-id="${t._id}">
+      <svg class="icon-trash" id="icon-trash-03" width="28" height="28">
+        <use href="/img/noptimizesprite.svg#icon-trash-03"></use>
+      </svg>
+    </button>
+      <img src="${t.book_image}" alt="${t.title}" class="shopping-item-img">
+      
+    
+    <div class="book-info-wrap">
+      <p class="book-title shopping-book-title break-text">${t.title}</p>
+      <p class="shopping-book-category break-text">${t.list_name}</p>
+      <p class="book-alt-text">${t.description}</p>
+      <div class="author-shops-wrap">
+        <p class="book-author">${t.author}</p>
+        <div class="img-wrap shopping-img-wrap">
+          <a href="${t.amazon_product_url}" target="blank">
+            <img src="${n}" alt="amazon-logo" class="amazon-logo shopping-amazon-logo" width="62" height="19">
+          </a>
+          <a href="${t.buy_links[1].url}" target="_blank">
+            <img src="${l}" alt="apple-book" class="apple-logo shopping-apple-logo" width="33" height="32">
+          </a>
+        </div>
+      </div>
+    </div>
+  </li>`}
+//# sourceMappingURL=commonHelpers2.js.map
