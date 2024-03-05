9463cfb1f4f07734f3e1f6d893571dff5ddd1f1c
DipsikEducation <144449101+DipsikEducation@users.noreply.github.com>
2024-03-05 12:35:16 +0000

    Deploying to gh-pages from @ DipsikEducation/kilobyte@165e72a9f2662157792c1683203c8b4a94437226 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,7 +1,7 @@
-import{g as i,d as r,i as p,e as c,h as d,r as e,o as g,t as h}from"./assets/localStorage-3158d51c.js";import"./assets/vendor-42c9f451.js";const m=document.querySelector("#shopping-list"),s=document.querySelector(".box-bookslist");a();function a(){i().length?s.classList.add("emptyShoppingList"):s.classList.remove("emptyShoppingList")}window.addEventListener("load",n);function n(){const t=i().map(l=>b(l)).join("");m.innerHTML=t,document.querySelector("#shopping-list").addEventListener("click",u)}function u(t){!t.target.hasAttribute("id")!=="icon-deleteBtn"&&(r(t),a(),n())}function b(t){return`<li class="shopping-list-item">
+import{g as i,d as r,i as c,e as p,h as d,r as o,o as h,t as g}from"./assets/localStorage-b811e2e4.js";import"./assets/vendor-42c9f451.js";const m=document.querySelector("#shopping-list"),s=document.querySelector(".box-bookslist");n();function n(){i().length?s.classList.add("emptyShoppingList"):s.classList.remove("emptyShoppingList")}window.addEventListener("load",a);function a(){const t=i().map(l=>L(l)).join("");m.innerHTML=t,document.querySelector("#shopping-list").addEventListener("click",u)}function u(t){!t.target.hasAttribute("id")!=="icon-deleteBtn"&&(r(t),n(),a())}function L(t){return`<li class="shopping-list-item">
     <button type="button" class="icon-deleteBtn" data-id="${t._id}">
       <svg class="icon-trash" id="icon-trash-03" width="28" height="28">
-        <use href="${p}#icon-trash-03"></use>
+        <use href="${c}#icon-trash-03"></use>
       </svg>
     </button>
       <img src="${t.book_image}" alt="${t.title}" class="shopping-item-img">
@@ -15,7 +15,7 @@ import{g as i,d as r,i as p,e as c,h as d,r as e,o as g,t as h}from"./assets/loc
         <p class="book-author">${t.author}</p>
         <div class="img-wrap shopping-img-wrap">
           <a href="${t.amazon_product_url}" target="blank">
-            <img src="${c}" alt="amazon-logo" class="amazon-logo shopping-amazon-logo" width="62" height="19">
+            <img src="${p}" alt="amazon-logo" class="amazon-logo shopping-amazon-logo" width="62" height="19">
           </a>
           <a href="${t.buy_links[1].url}" target="_blank">
             <img src="${d}" alt="apple-book" class="apple-logo shopping-apple-logo" width="33" height="32">
@@ -23,5 +23,5 @@ import{g as i,d as r,i as p,e as c,h as d,r as e,o as g,t as h}from"./assets/loc
         </div>
       </div>
     </div>
-  </li>`}window.addEventListener("resize",function(){L()});function L(){const t=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,o=document.querySelector(".support");t>1440&&o?o.style.display="block":o.style.display="none"}localStorage.getItem("theme")==="dark"&&(e.themeCheckbox.checked=!0,document.body.classList.add("dark-theme"));e.themeCheckbox.addEventListener("change",g);e.burgerBtn.addEventListener("click",()=>{e.burgerCont.classList.toggle("is-open-burger"),e.body.classList.toggle("modal-open-burger"),h()});
+  </li>`}window.addEventListener("resize",function(){b()});function b(){const t=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,e=document.querySelector(".support");t>1440&&e?e.style.display="block":e.style.display="none"}document.addEventListener("DOMContentLoaded",function(){const t=window.location.pathname,e=document.querySelector(".header-nav-shop");t.includes("shopinglist.html")&&e.classList.add("is-active")});localStorage.getItem("theme")==="dark"&&(o.themeCheckbox.checked=!0,document.body.classList.add("dark-theme"));o.themeCheckbox.addEventListener("change",h);o.burgerBtn.addEventListener("click",()=>{o.burgerCont.classList.toggle("is-open-burger"),o.body.classList.toggle("modal-open-burger"),g()});
 //# sourceMappingURL=commonHelpers2.js.map
