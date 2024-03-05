b08de2cabd3a1983198a246cf0db034323a9b485
DipsikEducation <144449101+DipsikEducation@users.noreply.github.com>
2024-03-05 11:19:19 +0000

    Deploying to gh-pages from @ DipsikEducation/kilobyte@4a2339d65ba9f733a7b9a110d686386fbbc64b74 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{g as i,d as l,i as p,e as r,h as c}from"./assets/localStorage-9b7c8adb.js";import"./assets/vendor-b7a1aa6d.js";const d=document.querySelector("#shopping-list"),e=document.querySelector(".box-bookslist");s();function s(){i().length?e.classList.add("emptyShoppingList"):e.classList.remove("emptyShoppingList")}window.addEventListener("load",n);function n(){const t=i().map(a=>h(a)).join("");d.innerHTML=t,document.querySelector("#shopping-list").addEventListener("click",g)}function g(t){!t.target.hasAttribute("id")!=="icon-deleteBtn"&&(l(t),s(),n())}function h(t){return`<li class="shopping-list-item">
+import{g as i,d as r,i as p,e as c,h as d,r as e,o as g,t as h}from"./assets/localStorage-3158d51c.js";import"./assets/vendor-42c9f451.js";const m=document.querySelector("#shopping-list"),s=document.querySelector(".box-bookslist");a();function a(){i().length?s.classList.add("emptyShoppingList"):s.classList.remove("emptyShoppingList")}window.addEventListener("load",n);function n(){const t=i().map(l=>b(l)).join("");m.innerHTML=t,document.querySelector("#shopping-list").addEventListener("click",u)}function u(t){!t.target.hasAttribute("id")!=="icon-deleteBtn"&&(r(t),a(),n())}function b(t){return`<li class="shopping-list-item">
     <button type="button" class="icon-deleteBtn" data-id="${t._id}">
       <svg class="icon-trash" id="icon-trash-03" width="28" height="28">
         <use href="${p}#icon-trash-03"></use>
@@ -15,13 +15,13 @@ import{g as i,d as l,i as p,e as r,h as c}from"./assets/localStorage-9b7c8adb.js
         <p class="book-author">${t.author}</p>
         <div class="img-wrap shopping-img-wrap">
           <a href="${t.amazon_product_url}" target="blank">
-            <img src="${r}" alt="amazon-logo" class="amazon-logo shopping-amazon-logo" width="62" height="19">
+            <img src="${c}" alt="amazon-logo" class="amazon-logo shopping-amazon-logo" width="62" height="19">
           </a>
           <a href="${t.buy_links[1].url}" target="_blank">
-            <img src="${c}" alt="apple-book" class="apple-logo shopping-apple-logo" width="33" height="32">
+            <img src="${d}" alt="apple-book" class="apple-logo shopping-apple-logo" width="33" height="32">
           </a>
         </div>
       </div>
     </div>
-  </li>`}window.addEventListener("resize",function(){m()});function m(){const t=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,o=document.querySelector(".support");t>1440&&o?o.style.display="block":o.style.display="none"}
+  </li>`}window.addEventListener("resize",function(){L()});function L(){const t=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,o=document.querySelector(".support");t>1440&&o?o.style.display="block":o.style.display="none"}localStorage.getItem("theme")==="dark"&&(e.themeCheckbox.checked=!0,document.body.classList.add("dark-theme"));e.themeCheckbox.addEventListener("change",g);e.burgerBtn.addEventListener("click",()=>{e.burgerCont.classList.toggle("is-open-burger"),e.body.classList.toggle("modal-open-burger"),h()});
 //# sourceMappingURL=commonHelpers2.js.map
