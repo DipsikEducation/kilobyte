4a2339d65ba9f733a7b9a110d686386fbbc64b74
DipsikKk <141740406+DearDany@users.noreply.github.com>
2024-03-05 13:19:01 +0200

    update

--- src/shoping-list-main.js
+++ src/shoping-list-main.js
@@ -1,3 +1,23 @@
 import './js/shoppingListRender'
 import './js/support'
+import './js/theme.js'
 
+import { refs } from './js/refs';
+import { onAddThemeLocalStorage } from './js/localStorage';
+import { toggleModal } from './js/modalburger.js';
+
+// Темна тема
+if (localStorage.getItem('theme') === 'dark') {
+  refs.themeCheckbox.checked = true;
+  document.body.classList.add('dark-theme');
+}
+
+refs.themeCheckbox.addEventListener('change', onAddThemeLocalStorage);
+
+// Бургер меню
+
+refs.burgerBtn.addEventListener('click', () => {
+  refs.burgerCont.classList.toggle('is-open-burger');
+  refs.body.classList.toggle('modal-open-burger');
+  toggleModal();
+});
